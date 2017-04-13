8185c65b6d1c19fbcf842e8b267561ffa29fcd74
Matous Jobanek <mjobanek@redhat.com>
2017-04-13 08:37:22 +0200

    initial commit

---
+++ src/main/java/my/k8s/client/MyClient.java
@@ -0,0 +1,24 @@
+package my.k8s.client;
+
+import io.fabric8.kubernetes.api.model.NamespaceList;
+import io.fabric8.kubernetes.api.model.ServiceList;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+
+public class MyClient {
+
+    public static void main(String[] args){
+
+        ///home/mjobanek/cert/rootCA.key
+        Config config = new ConfigBuilder().withMasterUrl("https://192.168.99.100:8443/").withCaCertFile("/home/mjobanek/cert/ca.crt").build();
+
+        DefaultKubernetesClient client = new DefaultKubernetesClient(config);
+
+        NamespaceList namespaceList = client.namespaces().list();
+        ServiceList serviceList = client.services().list();
+        ServiceList defaultServicesList = client.services().inNamespace("default").list();
+
+
+    }
+}
